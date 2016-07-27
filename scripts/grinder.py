5f4cddf41397ede14246878a3f4ae82beb1c0085
Richard Sartor <richard.sartor@rackspace.com>
2016-07-27 12:20:40 -0500

    Fix the import.

--- scripts/grinder.py
+++ scripts/grinder.py
@@ -5,13 +5,14 @@ import annotationsingest
 import query
 import utils
 import thread_manager as tm
+import py_java
 
 # ENTRY POINT into the Grinder
 
 # The code inside the TestRunner class is gets executed by each worker thread
 # Outside the class is executed before any of the workers begin
 
-config = utils.get_config_from_grinder(grinder)
+config = py_java.get_config_from_grinder(grinder)
 thread_manager = tm.ThreadManager(config)
 thread_manager.create_all_metrics(grinder.getAgentNumber())
 
