cd355ceb502329cbe0994b685c45f7507d60c496
Richard Sartor <richard.sartor@rackspace.com>
2016-07-29 12:15:56 -0500

    Move thread manager initialization into the TestRunner class.

--- scripts/grinder.py
+++ scripts/grinder.py
@@ -11,13 +11,12 @@ import py_java
 # The code inside the TestRunner class is gets executed by each worker thread
 # Outside the class is executed before any of the workers begin
 
-config = py_java.get_config_from_grinder(grinder)
-thread_manager = tm.ThreadManager(config)
-thread_manager.create_all_metrics(grinder.getAgentNumber())
-
 
 class TestRunner:
     def __init__(self):
+        config = py_java.get_config_from_grinder(grinder)
+        thread_manager = tm.ThreadManager(config)
+        thread_manager.create_all_metrics(grinder.getAgentNumber())
         self.thread = thread_manager.setup_thread(grinder.getThreadNumber())
 
     def __call__(self):
