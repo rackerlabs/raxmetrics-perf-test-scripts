331a69ac7c30ece383858caa4c82d26ae05aa085
Richard Sartor <richard.sartor@rackspace.com>
2016-08-08 14:29:00 -0500

    Remove unused functions.

--- scripts/abstract_thread.py
+++ scripts/abstract_thread.py
@@ -66,41 +66,5 @@ class AbstractThread(object):
         return time.sleep(x / 1000)
 
 
-def shuffled(unshuffled):
-    lst = list(unshuffled)
-    random.shuffle(lst)
-    return lst
-
-
-def generate_job_range(total_jobs, total_servers, server_num):
-    """ Determine which subset of the total work the current server is to
-    do.
-
-    The properties file is the same for all the distributed workers and
-    lists the total amount of work to be done for each report interval.
-    This method allows you to split that work up into the exact subset to
-    be done by the "server_num" worker
-    """
-    jobs_per_server = total_jobs / total_servers
-    remainder = total_jobs % total_servers
-    start_job = jobs_per_server * server_num
-    start_job += min(remainder, server_num)
-    end_job = start_job + jobs_per_server
-    if server_num < remainder:
-        end_job += 1
-    return (start_job, end_job)
-
-
-def generate_metrics_tenants(num_tenants, metrics_per_tenant,
-                             agent_number, num_nodes, gen_fn):
-    """generate the subset of the total metrics to be done by this agent"""
-    tenants_in_shard = range(
-        *generate_job_range(num_tenants, num_nodes, agent_number))
-    metrics = []
-    for y in [gen_fn(x, metrics_per_tenant) for x in tenants_in_shard]:
-        metrics += y
-    return shuffled(metrics)
-
-
 def generate_metric_name(metric_id, config):
     return config['name_fmt'] % metric_id
