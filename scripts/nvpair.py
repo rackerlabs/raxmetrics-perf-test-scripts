0f01766c6615e02a6229aa430c8ead63bb518bcf
Richard Sartor <richard.sartor@rackspace.com>
2016-10-13 14:15:54 -0500

    Replace the stand-in NVPair function with a class.

--- scripts/nvpair.py
+++ scripts/nvpair.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python
 
 
-def NVPair(*args):
+class NVPair(object):
     """
     Normally, one would import the NVPair class from Grinder's HTTPClient
     module. However, when trying to run the tests in an IDE or under python
-    (not jython), HTTPClient is unavailable. In those situations, this function
+    (not jython), HTTPClient is unavailable. In those situations, this class
     can serve as a simple stand-in.
     """
-    return tuple(args)
+
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+
+    def getName(self):
+        return self.name
+
+    def getValue(self):
+        return self.value
