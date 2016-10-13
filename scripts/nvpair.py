54ee3d5c04cc603a462204c20e8ebc3ee23e110e
Richard Sartor <richard.sartor@rackspace.com>
2016-10-13 11:11:42 -0500

    Explain the NVPair function.

--- scripts/nvpair.py
+++ scripts/nvpair.py
@@ -2,4 +2,10 @@
 
 
 def NVPair(*args):
+    """
+    Normally, one would import the NVPair class from Grinder's HTTPClient
+    module. However, when trying to run the tests in an IDE or under python
+    (not jython), HTTPClient is unavailable. In those situations, this function
+    can serve as a simple stand-in.
+    """
     return tuple(args)
