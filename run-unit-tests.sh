e9e92d1f77919f196ef3044b523bf85f10885fed
Richard Sartor <richard.sartor@rackspace.com>
2016-10-14 11:08:49 -0500

    Rename the properties file used for unit tests.

--- run-unit-tests.sh
+++ run-unit-tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 . ./set-env.sh
-java -cp  dependencies/grinder-3.11/lib/grinder.jar:dependencies/jyson-1.0.2/lib/jyson-1.0.2.jar net.grinder.Grinder properties/grinder.properties
+java -cp  dependencies/grinder-3.11/lib/grinder.jar:dependencies/jyson-1.0.2/lib/jyson-1.0.2.jar net.grinder.Grinder properties/grinder-unittests.properties
