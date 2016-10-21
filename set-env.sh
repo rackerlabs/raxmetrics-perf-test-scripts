6161b832cd65d41636e496093bc78d31c8124a07
Richard Sartor <richard.sartor@rackspace.com>
2016-10-21 12:33:17 -0500

    Fix it so that set-env.sh also works when sourced from other scripts.

--- set-env.sh
+++ set-env.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-topsrc=`dirname $0`  # this gives you the directory where this script is located
+called=$_
+topsrc=`dirname $called`  # this gives you the directory where this script is located
 export CLASSPATH=$topsrc/dependencies/grinder-3.11/lib/*:$topsrc/dependencies/jyson-1.0.2/lib/*:$topsrc/dependencies/jasypt-1.9.2/lib/*
