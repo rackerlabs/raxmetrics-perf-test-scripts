f1deb061eef44cab7dcdd4da69494163044c171f
Richard Sartor <richard.sartor@rackspace.com>
2016-11-22 10:38:27 -0600

    Include newer modules in coverage numbers.

--- run-unit-tests-with-coverage.sh
+++ run-unit-tests-with-coverage.sh
@@ -2,6 +2,6 @@
 
 topsrc=`dirname $0`  # this gives you the directory where this script is located
 cd $topsrc
-coverage run --source=abstract_thread,annotationsingest,ingest,ingestenum,query,thread_manager,config,throttling_group scripts/tests.py
+coverage run --source=abstract_thread,annotationsingest,authenticating_request,config,connector,exception_handling_request,get_user,grinder,grinder_connector,http_failure_exception,ingest,ingestenum,nvpair,py_java,query,requests_connector,response_checking_request,thread_manager,throttling_group,throttling_request,user scripts/tests.py
 coverage html
 
