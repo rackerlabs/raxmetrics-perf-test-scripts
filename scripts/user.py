89db5c39292d817fbbba3e21cf526fcedac45eee
Richard Sartor <richard.sartor@rackspace.com>
2016-11-22 10:27:17 -0600

    Move the _get_data_sync method into _get_data. No other code needs to be able to call it.

--- scripts/user.py
+++ scripts/user.py
@@ -29,56 +29,57 @@ class User(object):
         self.lock = threading.RLock()
 
     def _get_data(self):
-        self.lock.acquire()
-        try:
-            if self.token is None or self.tenant_id is None:
-                return self._get_data_sync()
-        finally:
-            self.lock.release()
-        return self.tenant_id, self.token
-
-    def _get_data_sync(self):
-        request_body = {
-            "auth": {
-                "RAX-KSKEY:apiKeyCredentials": {
-                    "username": self.username,
-                    "apiKey": self.api_key}}}
-        headers = [
-            NVPair("Accept", "application/json"),
-            NVPair("Content-type", "application/json")
-        ]
-        for retries in xrange(3):
-            resp = self.connector.post(self.auth_url, request_body, headers)
-            if 200 <= resp.status_code < 300:
-                break
-            if resp.status_code in [413, 429]:
-                try:
-                    # TODO: lower/upper case
-                    retry_after = resp.headers.get('Retry-After')
-                    if retry_after:
-                        # TODO: double-check time zone
-                        after = email.utils.parsedate(retry_after)
-                        now = datetime.datetime.utcnow()
-                        delta = (after - now).total_seconds()
-                        if delta > 0:
-                            self.logger('got status code %s, will retry' %
-                                        resp.status_code)
-                            time.sleep(delta)
-                except:
-                    self.logger('got status code %s, can\'t retry' %
+        def get_data_sync():
+            request_body = {
+                "auth": {
+                    "RAX-KSKEY:apiKeyCredentials": {
+                        "username": self.username,
+                        "apiKey": self.api_key}}}
+            headers = [
+                NVPair("Accept", "application/json"),
+                NVPair("Content-type", "application/json")
+            ]
+            for retries in xrange(3):
+                resp = self.connector.post(self.auth_url, request_body,
+                                           headers)
+                if 200 <= resp.status_code < 300:
+                    break
+                if resp.status_code in [413, 429]:
+                    try:
+                        # TODO: lower/upper case
+                        retry_after = resp.headers.get('Retry-After')
+                        if retry_after:
+                            # TODO: double-check time zone
+                            after = email.utils.parsedate(retry_after)
+                            now = datetime.datetime.utcnow()
+                            delta = (after - now).total_seconds()
+                            if delta > 0:
+                                self.logger('got status code %s, will retry' %
+                                            resp.status_code)
+                                time.sleep(delta)
+                    except:
+                        self.logger('got status code %s, can\'t retry' %
+                                    resp.status_code)
+                        return (None, None)
+                else:
+                    self.logger('got status code %s, won\'t retry' %
                                 resp.status_code)
                     return (None, None)
             else:
-                self.logger('got status code %s, won\'t retry' %
-                            resp.status_code)
+                self.logger('Couldn\'t get token and tenant')
                 return (None, None)
-        else:
-            self.logger('Couldn\'t get token and tenant')
-            return (None, None)
 
-        catalog = resp.json()
-        self.tenant_id = catalog['access']['token']['tenant']['id']
-        self.token = catalog['access']['token']['id']
+            catalog = resp.json()
+            self.tenant_id = catalog['access']['token']['tenant']['id']
+            self.token = catalog['access']['token']['id']
+            return self.tenant_id, self.token
+
+        self.lock.acquire()
+        try:
+            if self.token is None or self.tenant_id is None:
+                return get_data_sync()
+        finally:
+            self.lock.release()
         return self.tenant_id, self.token
 
     def get_token(self):
