d42b2cf29073e481e00902cd8988ff2acbaaa9a3
Richard Sartor <richard.sartor@rackspace.com>
2017-02-23 14:51:47 -0600

    Make the whole script location-independent.

--- setup-dependencies.sh
+++ setup-dependencies.sh
@@ -3,12 +3,11 @@
 topsrc="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"  # this gives you the directory where this script is located
 
 mkdir -p $topsrc/dependencies
-cd $topsrc/dependencies
 
-unzip grinder-3.11-binary.zip
-chmod 755 grinder-3.11
+unzip $topsrc/dependencies/grinder-3.11-binary.zip -d $topsrc/dependencies/
+chmod 755 $topsrc/dependencies/grinder-3.11
 
-unzip jyson-1.0.2.zip
+unzip $topsrc/dependencies/jyson-1.0.2.zip -d $topsrc/dependencies/
 
-unzip jasypt-1.9.2-dist.zip
-find jasypt-1.9.2 -type d -exec chmod 755 {} +
+unzip $topsrc/dependencies/jasypt-1.9.2-dist.zip -d $topsrc/dependencies/
+find $topsrc/dependencies/jasypt-1.9.2 -type d -exec chmod 755 {} +
