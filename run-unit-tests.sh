25f4e51c95035115408bb90497ce9d14dcd46299
Richard Sartor <richard.sartor@rackspace.com>
2017-02-27 10:42:14 -0600

    Use the absolute path to the properties file.

--- run-unit-tests.sh
+++ run-unit-tests.sh
@@ -2,4 +2,4 @@
 
 topsrc="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"  # this gives you the directory where this script is located
 . $topsrc/set-env.sh
-java net.grinder.Grinder properties/grinder-unittests.properties
+java net.grinder.Grinder $topsrc/properties/grinder-unittests.properties
