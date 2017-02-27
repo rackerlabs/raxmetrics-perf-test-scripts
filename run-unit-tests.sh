23778dc81b4a5a4bb26365e5ffea9942a0c0fa4a
Richard Sartor <richard.sartor@rackspace.com>
2017-02-27 10:39:22 -0600

    Don't cd to the directory.

--- run-unit-tests.sh
+++ run-unit-tests.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 topsrc="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"  # this gives you the directory where this script is located
-cd $topsrc
 . $topsrc/set-env.sh
 java -cp  dependencies/grinder-3.11/lib/grinder.jar:dependencies/jyson-1.0.2/lib/jyson-1.0.2.jar net.grinder.Grinder properties/grinder-unittests.properties
