af7a81ac70264a5fb66d584d68f9bb80ec34d803
Richard Sartor <richard.sartor@rackspace.com>
2017-04-20 12:57:30 -0500

    Process multiple log files from the command line.

--- util/process_log.py
+++ util/process_log.py
@@ -2,14 +2,13 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--num-tests', action='store', default=6)
-parser.add_argument('--log-file', default='first-24-hours-data.log')
 parser.add_argument('--start-time-ms', default=0)
 parser.add_argument('--stop-time-ms', default=2000000000000)
 # 2000000000000ms --> May 17th, 2033 at 10:33:20 PM
+parser.add_argument('log_files', nargs='+', metavar='log-files')
 
 args = parser.parse_args()
 
-print('Log file: {}'.format(args.log_file))
 print('Num. tests: {}'.format(args.num_tests))
 print('Start time: {} ms'.format(args.start_time_ms))
 print('Stop time: {} ms'.format(args.stop_time_ms))
@@ -29,55 +28,56 @@ last_line_number = 'none'
 def clean_int(x):
     return int(x.strip())
 
-
-with open(args.log_file) as f:
-    header = f.readline()
-    total_lines = 1
-    for line in f.readlines():
-        total_lines += 1
-        (thread, run, test, test_start_time, test_time, errors, response_code,
-         response_length, response_errors, time_to_resolv, time_to_connect,
-         time_to_first_byte, new_conns) = map(clean_int, line.split(','))
-        if start_time <= test_start_time < stop_time:
-            if not first_line:
-                first_line = line
-                first_line_number = total_lines
-
-            test_counts[test] += 1
-            if int(errors) < 1:
-                test_success_counts[test] += 1
-
-            last_line = line
-            last_line_number = total_lines
-
-total = sum(test_counts.itervalues())
-total_successes = sum(test_success_counts.itervalues())
-
-
 def percent(x, n):
     try:
         return int(round(100 * x / n))
     except:
         return float('nan')
 
+print args
+for log_file in args.log_files:
+    with open(log_file) as f:
+        header = f.readline()
+        total_lines = 1
+        for line in f.readlines():
+            total_lines += 1
+            (thread, run, test, test_start_time, test_time, errors, response_code,
+             response_length, response_errors, time_to_resolv, time_to_connect,
+             time_to_first_byte, new_conns) = map(clean_int, line.split(','))
+            if start_time <= test_start_time < stop_time:
+                if not first_line:
+                    first_line = line
+                    first_line_number = total_lines
 
-print('Totals: {}'.format(total))
-for test in sorted(test_counts.iterkeys()):
-    print('  {}: {} ({}% of all requests)'.format(test, test_counts[test],
-                                                  percent(test_counts[test],
-                                                          total)))
+                test_counts[test] += 1
+                if int(errors) < 1:
+                    test_success_counts[test] += 1
 
-print('Successes: {}'.format(total_successes))
-for test in sorted(test_success_counts.iterkeys()):
-    template = ('  {}: {} ({}% of all successful requests,'
-                ' {}% success rate for this test)')
-    print(template.format(test, test_success_counts[test],
-                          percent(test_success_counts[test],
-                                  total_successes),
-                          percent(test_success_counts[test],
-                                  test_counts[test])))
+                last_line = line
+                last_line_number = total_lines
 
-print('')
-print('Total lines: {}'.format(total_lines))
-print('First line: "{}" ({})'.format(first_line.strip(), first_line_number))
-print('Last line: "{}" ({})'.format(last_line.strip(), last_line_number))
+    total = sum(test_counts.itervalues())
+    total_successes = sum(test_success_counts.itervalues())
+
+    print('Log file: {}'.format(args.log_file))
+
+    print('  Totals: {}'.format(total))
+    for test in sorted(test_counts.iterkeys()):
+        print('    {}: {} ({}% of all requests)'.format(test, test_counts[test],
+                                                      percent(test_counts[test],
+                                                              total)))
+
+    print('  Successes: {}'.format(total_successes))
+    for test in sorted(test_success_counts.iterkeys()):
+        template = ('    {}: {} ({}% of all successful requests,'
+                    ' {}% success rate for this test)')
+        print(template.format(test, test_success_counts[test],
+                              percent(test_success_counts[test],
+                                      total_successes),
+                              percent(test_success_counts[test],
+                                      test_counts[test])))
+
+    print('')
+    print('  Total lines: {}'.format(total_lines))
+    print('  First line: "{}" ({})'.format(first_line.strip(), first_line_number))
+    print('  Last line: "{}" ({})'.format(last_line.strip(), last_line_number))
