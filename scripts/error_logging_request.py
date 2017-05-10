008b941606504c4d28b065815176e02ad531bc28
Richard Sartor <richard.sartor@rackspace.com>
2017-05-10 19:45:21 +0000

    Log the specific parts of the response, instead of just the string representation of the object.

--- scripts/error_logging_request.py
+++ scripts/error_logging_request.py
@@ -12,7 +12,17 @@ class ErrorLoggingRequest():
     def wrap(self, callee):
         response = callee()
         if response.getStatusCode() >= 500:
-            self.log(response)
+            s = '\n    %s %s %s\n' % (response.getVersion(), response.getStatusCode(),
+                                response.getReasonLine())
+            def clean(s):
+                return s.replace('\n', '\n    ')
+            for header in response.listHeaders():
+                s += '    %s: %s\n' % (header, clean(response.getHeader(header)))
+            text = response.getText()
+            if text:
+                s += '\n    '
+                s += clean(text)
+            self.log(s)
         return response
 
     def GET(self, *args, **kwargs):
